d7185e1e63c11be4841b50482fd38712eaee3f6a
Sue Roberts <suerobertstx@yahoo.com>
2022-04-08 18:43:34 -0500

    Fix: User interface logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,53 @@
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
-    event.preventDefault();
 
-    const nameInput = $("input#name").val();
-    const code=parseInt($("input#code").val());
+    const yourNameInput=$("input#yourName").val();
+    const code=$("#code").val();
     const color=$("input:radio[name=color]:checked").val();
     const book=$("input:radio[name=book]:checked").val();
     const activity=$("input:radio[name=activity]:checked").val();
     const food=$("#food").val();
     
-    $(".name").text(nameInput);
-    $("userName").show();
-    if (code>=1 && code<=5){
-      if (color==="red" && "book===yes"){
+    $(".yourName").text(yourNameInput);
+    $("#userName").show();
+    
+    if(code==="yes"  && color==="red"){
       $("#python").show();
       $("#ruby").hide();
       $("#c").hide();
       $("#diffOption").hide();
-    }else if (color==="pink"){
+    }else if(code==="no" && color==="pink"){
+      $("#ruby").show();
+      $("#python").hide();
+      $("#javascript").hide();
+      $("#trouble").hide();
+    }else if(color==="blue" && code==="yes"){
       $("#c").show();
       $("#ruby").hide();
       $("#diffOption").hide();
       $("#python").hide();
-    }else if (book==="yes"){
+    }else if(code==="yes" && color==="pink"){
       $("#ruby").show();
       $("##diffOption").hide();
       $("#c").hide();
       $("#python").hide();
-    }else if (activity==="games" && food==="american"){
-      $("#ruby").show();
-      $("##diffOption").hide();
+    }else if(code==="no" && color==="red"){
+      $("#python").show();
+      $("#ruby").hide();
       $("#c").hide();
-      $("#python").hide();  
-    }else {
-    $("#ruby").hide();
-    $("##diffOption").show();
-    $("#c").hide();
-    $("#python").hide();
+      $("#diffOption").hide();
+    }else if(code==="no" &&  color==="blue"){
+      $("#c").show();
+      $("#ruby").hide();
+      $("#diffOption").hide();
+      $("#python").hide();
+    }else{
+      $("#ruby").hide();
+      $("##diffOption").show();
+      $("#c").hide();
+      $("#python").hide();
     }
-  }
+   
+    event.preventDefault();
   });
 });
